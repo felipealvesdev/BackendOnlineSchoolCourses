7ba3af662dd6572723927c64ccca231a677d0b10
Felipe Alves <felipealexandre1408@gmail.com>
2024-12-26 14:36:22 -0300

    First commit

---
+++ src/main/java/com/onlineSchool/onlineSchoolCourses/OnlineSchoolCoursesApplication.java
@@ -0,0 +1,13 @@
+package com.onlineSchool.onlineSchoolCourses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineSchoolCoursesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineSchoolCoursesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onlineSchool/onlineSchoolCourses/OnlineSchoolCoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlineSchool.onlineSchoolCourses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineSchoolCoursesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
