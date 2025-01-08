984cb5e066496e3514d776d24fce1a3ae218f6e1
Felipe Alves <felipealexandre1408@gmail.com>
2025-01-07 21:10:44 -0300

    Feat: adding security configuration to the project but there is a minor bug in the class User.

--- src/main/java/com/onlineSchool/onlineSchoolCourses/domain/User.java
+++ src/main/java/com/onlineSchool/onlineSchoolCourses/domain/User.java
@@ -6,7 +6,11 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
 
 @Entity(name = "users")
@@ -14,8 +18,8 @@ import java.util.List;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode
-public class User {
+@EqualsAndHashCode(of = "id")
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,4 +34,42 @@ public class User {
 
     @OneToMany(mappedBy = "student", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Progress> progressList;
+
+    public User(String email, String password, UserRole userRole) {
+        this.email = email;
+        this.password = password;
+        this.userRole = userRole;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if(this.userRole == UserRole.INSTRUCTOR) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"),
+                new SimpleGrantedAuthority("ROLE_USER"));
+        else return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- src/main/java/com/onlineSchool/onlineSchoolCourses/repositories/UserRepository.java
+++ src/main/java/com/onlineSchool/onlineSchoolCourses/repositories/UserRepository.java
@@ -2,6 +2,11 @@ package com.onlineSchool.onlineSchoolCourses.repositories;
 
 import com.onlineSchool.onlineSchoolCourses.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    UserDetails findByLogin(String login);
+    User findUserRoleByLogin(String login);
+
 }
